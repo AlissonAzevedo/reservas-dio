658584e1ad65967e48c2fc0fb7753288047bb4f4
AlissonAzevedo <61812126+AlissonAzevedo@users.noreply.github.com>
2022-03-03 11:08:51 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,6 @@
     <div>
       <MenuLateral />
     </div>
-    <!-- <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>-->
     <router-view />
   </div>
 </template>
