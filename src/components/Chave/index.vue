a92ae359850c6e8b70d41905a8e142b02e2b6e69
AlissonAzevedo <61812126+AlissonAzevedo@users.noreply.github.com>
2022-03-03 11:06:21 -0300

    feat: add component chave

---
+++ src/components/Chave/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+        <div class="icon">
+            <KeyIcon :size="24"/>
+        </div>
+        <p>CHAVE</p>
+    </div>
+</template>
+
+<script>
+import KeyIcon from 'vue-material-design-icons/KeyVariant'
+export default {
+    name: "Chave",
+    components: {
+        KeyIcon,
+    },
+};   
+
+</script>
+
+<style scoped>
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+}
+
+.container:hover {
+    background: rgba(255, 255, 255, 0.2);
+}
+.icon{
+    transform:rotateY(180deg);
+}
+</style>
