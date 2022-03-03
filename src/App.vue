2b4718db1e2da7262290826d61cad230ba1d859b
AlissonAzevedo <61812126+AlissonAzevedo@users.noreply.github.com>
2022-03-03 11:06:51 -0300

    chore: add routes

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,42 @@
 <template>
-  <nav>
+  <div class="container">
+    <div>
+      <MenuLateral />
+    </div>
+    <!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  </nav>-->
+    <router-view />
+  </div>
 </template>
-
+<script>
+import MenuLateral from "./components/MenuLateral.vue";
+export default {
+  name: "App",
+  components: {
+    MenuLateral,
+  },
+};
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+:root {
+  --green: #42b983;
+  --primary: #2c3e50;
+  --background-menu: #794150;
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Rubik', sans-serif;
+}
+.container {
+  display: grid;
+  height: 100vh;
+  grid-template-columns: 80px auto;  /* 80px = width do menu lateral */
+  grid-template-rows: 100%;
+  grid-template-areas: "menu-lateral content";
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
