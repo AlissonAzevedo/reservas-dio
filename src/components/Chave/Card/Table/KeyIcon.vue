490f259323e7414f0767071e944aaf8d5f3f7d98
AlissonAzevedo <61812126+AlissonAzevedo@users.noreply.github.com>
2022-03-13 12:46:07 -0300

    style: add KeyIcon

---
+++ src/components/Chave/Card/Table/KeyIcon.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <img src="icon/icon_key.svg" alt="key" class="icon" >
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Icon",
+};
+</script>
+
+<style scoped>
+.icon {
+    width: 16px;
+    height: 16px;
+    margin-right: 2px;
+}
+</style>
