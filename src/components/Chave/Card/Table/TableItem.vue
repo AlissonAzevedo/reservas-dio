37fe84d4791c87de69959b3fc42f8b3a57633352
AlissonAzevedo <61812126+AlissonAzevedo@users.noreply.github.com>
2022-03-13 17:06:17 -0300

    chore: add props

--- src/components/Chave/Card/Table/TableItem.vue
+++ src/components/Chave/Card/Table/TableItem.vue
@@ -1,41 +1,72 @@
 <template>
-    <div class="table-item">
-        <ul>
-            <li>Alisson da Silva Azevedo</li>
-            <li>140,143</li>
-            <li>23/02/2002 - 06:57</li>
-            <li>23/02/2002 - 12:02</li>
-        </ul>
-    </div>
+  <div class="table-item">
+    <ul>
+      <li>{{ name }}</li>
+      <li>
+        <KeyIcon />
+        {{ keys }}
+      </li>
+      <li>{{ delivered }}</li>
+      <li>{{ devolution }}</li>
+      <li><EditIcon /></li>
+      <li><RemoveIcon /></li>
+    </ul>
+  </div>
 </template>
 
 <script>
-    export default {
-       name: "Table", 
-    }
+import KeyIcon from "./Icons/KeyIcon.vue";
+import EditIcon from "./Icons/EditIcon.vue";
+import RemoveIcon from "./Icons/RemoveIcon.vue";
+export default {
+  name: "Table",
+  components: {
+    KeyIcon,
+    EditIcon,
+    RemoveIcon,
+  },
+  props: {
+    name: {
+      type: String,
+      default: "Fulano de Tal",
+    },
+    keys: {
+      type: String,
+      default: "001",
+    },
+    delivered: {
+      type: String,
+      default: "01/01/2001 - 06:57",
+    },
+    devolution: {
+      type: String,
+      default: "01/01/2001 - 12:02",
+    },
+  },
+};
 </script>
 
 <style scoped>
-
-ul{
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-around;
-    margin-top: 10px;
-    background-color:#E5E5E5;
-    border-radius: 8px;
-    height: 60px;
-    margin: 5px 15px;
+ul {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  margin-top: 10px;
+  background-color: #e5e5e5;
+  border-radius: 8px;
+  height: 60px;
+  margin: 5px 15px;
 }
 
-ul:hover{
-    background: rgba(0, 0, 0, 0.15);
+ul:hover {
+  background: rgba(0, 0, 0, 0.15);
 }
 
-li{
-    list-style: none;
-    color: #794150;
-    
+li {
+  list-style: none;
+  color: #794150;
+  display: flex;
+  flex-direction: row;
 }
 </style>
--- src/components/Chave/Card/index.vue
+++ src/components/Chave/Card/index.vue
@@ -1,28 +1,35 @@
 <template>
-    <div class="card">
-        <Table />
-        <TableItem />
-        <TableItem />
-        <TableItem />
-    </div>
+  <div class="card">
+    <Table />
+    <TableItem
+      name="Alisson Azevedo"
+      keys="140"
+      delivered="13/03/2022 - 06:57"
+      devolution="13/03/2022 - 12:07"
+    />
+    <TableItem name="João Amorim" />
+    <TableItem name="Marcos Frazão" />
+    <TableItem />
+    <TableItem />
+  </div>
 </template>
 
 <script>
-import Table from './Table'
-import TableItem from './Table/TableItem.vue'
+import Table from "./Table";
+import TableItem from "./Table/TableItem.vue";
 export default {
-    name: 'KeyCard',
-    components: {
-        Table,
-        TableItem
-    },
-}
+  name: "KeyCard",
+  components: {
+    Table,
+    TableItem,
+  },
+};
 </script>
 
 <style scoped>
-.card{
-    grid-area: KeyCard;
-    background-color: #fff;
-    border-radius: 8px;
+.card {
+  grid-area: KeyCard;
+  background-color: #fff;
+  border-radius: 8px;
 }
 </style>
