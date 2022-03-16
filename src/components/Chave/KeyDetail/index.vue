0f39fe6ed048c0b67ea5445d38099ca77f330368
AlissonAzevedo <61812126+AlissonAzevedo@users.noreply.github.com>
2022-03-16 10:36:50 -0300

    chore: add new component KeyDetail

---
+++ src/components/Chave/KeyDetail/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="card">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "KeyDetail",
+  data() {
+    return {
+    };
+  },
+  components: {},
+};
+</script>
+
+<style scoped>
+.card {
+  grid-area: KeyCard;
+  background-color: #fff;
+  border-radius: 8px;
+  overflow: scroll;
+  overflow-x: hidden !important;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.25);
+}
+.card::-webkit-scrollbar {
+  width: 8px;
+  background-color: #fff;
+  border-radius: 8px;
+}
+.card::-webkit-scrollbar-thumb {
+  background-color: #e5e5e5;
+  border-radius: 6px;
+}
+</style>
