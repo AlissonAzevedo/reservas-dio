efbd2b762dbeb0614916491bf05a69e383bf6097
AlissonAzevedo <61812126+AlissonAzevedo@users.noreply.github.com>
2022-03-16 10:36:27 -0300

    chore: add new view KeyDetail

---
+++ src/views/ChaveDetail.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container-card">
+    <div id="container-key">
+      <KeyDetail />
+    </div>
+  </div>
+</template>
+
+<script>
+import KeyDetail from "@/components/Chave/KeyDetail";
+
+export default {
+  name: "ChaveView",
+  components: {
+    KeyDetail,
+  },
+};
+</script>
+
+<style scoped>
+#container-key {
+  display: grid;
+  height: 100vh;
+  grid-template-columns: 70vw 20vw;
+  grid-template-rows: 70px 85vh;
+  grid-template-areas:
+    "SearchField SearchField "
+    "KeyCard CardAside";
+}
+.container-card {
+  background-color: var(--background-main);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
