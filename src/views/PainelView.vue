ee1fa0a110f2bea5835bd29664816ecce0c6bab3
AlissonAzevedo <61812126+AlissonAzevedo@users.noreply.github.com>
2022-03-26 11:29:34 -0300

    style: add página de manutenção

--- src/views/PainelView.vue
+++ src/views/PainelView.vue
@@ -1,18 +1,53 @@
 <template>
   <div class="container">
-    painel
+    <h1>Página em Manuteção</h1>
+
+    <div class="msg">
+      <img
+        src="https://i.ibb.co/qDpFTjz/settings.png"
+        alt="settings"
+        border="0"
+      />
+    </div>
+    <h3>
+      Clique em
+      <router-link to="/chaves">Chave</router-link>
+      para visualizar a página de chaves.
+    </h3>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'PainelView',
-  
-}
+  name: "PainelView",
+};
 </script>
 <style scoped>
 .container {
+  background-color: var(--background-main);
   display: grid;
-
+  justify-content: center;
+  align-content: center;
+}
+.msg {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+}
+.msg img {
+  width: 250px;
+  height: 250px;
+}
+h1 {
+  font-size: 60px;
+  color: var(--text);
+  text-align: center;
+  margin-bottom: 30px;
+}
+h3 {
+  font-size: 20px;
+  color: var(--text);
+  text-align: center;
+  margin-top: 30px;
 }
 </style>
